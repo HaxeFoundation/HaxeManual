98c58883e942465421c84480db1bed6b2cf7daf5
Simon Krajewski <simon@haxe.org>
2013-09-09 15:59:59 +0200

    macro arguments

---
+++ assets/MacroArgumentsConst.hx
@@ -0,0 +1,13 @@
+class Main {
+	static public function main() {
+		const("foo", 1, 1.5, true);
+	}
+	
+	macro static function const(s:String, i:Int, f:Float, b:Bool) {
+		trace(s);
+		trace(i);
+		trace(f);
+		trace(b);
+		return macro null;
+	}
+}
