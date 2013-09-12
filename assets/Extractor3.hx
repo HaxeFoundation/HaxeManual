a3440d74263009197b7038f9f72b9114cf5fde46
Simon Krajewski <simon@haxe.org>
2013-09-12 13:42:45 +0200

    extractors

---
+++ assets/Extractor3.hx
@@ -0,0 +1,19 @@
+enum Test {
+	TString(s:String);
+	TInt(i:Int);
+}
+
+class Main {
+	static public function main() {
+		var e = TInt(4);
+		var success = switch(e) {
+			case TInt(lessThan.bind(_, 5) => true): true;
+			case _: false;
+		}
+		trace(success);
+	}
+	
+	static function lessThan(lhs:Int, rhs:Int) {
+		return lhs < rhs;
+	}
+}
