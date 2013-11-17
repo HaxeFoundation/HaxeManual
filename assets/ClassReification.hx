a70e9c2b69ed6218de3366ff974f8247de3b9b52
Simon Krajewski <simon@haxe.org>
2013-11-17 11:16:03 +0100

    class reification (closes #23)

---
+++ assets/ClassReification.hx
@@ -0,0 +1,17 @@
+class Main {
+	macro static function generateClass(funcName:String) {
+		var c = macro class MyClass {
+			public function new() { }
+			public function $funcName() {
+				trace($v{funcName} + " was called");
+			}
+		}
+		haxe.macro.Context.defineType(c);
+		return macro new MyClass();
+	}
+	
+    public static function main() {
+		var c = generateClass("myFunc");
+		c.myFunc();
+    }
+}
