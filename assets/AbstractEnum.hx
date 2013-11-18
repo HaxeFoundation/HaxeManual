a17f4bc1f2d5f35ec93edab422f67e5519f6e036
Simon Krajewski <simon@haxe.org>
2013-11-18 19:26:19 +0100

    document @:enum abstract (closes #26)

---
+++ assets/AbstractEnum.hx
@@ -0,0 +1,19 @@
+@:enum
+abstract HttpStatus(Int) {
+	var NotFound = 404;
+	var MethodNotAllowed = 405;
+}
+
+class Main {
+	static public function main() {
+		var status = HttpStatus.NotFound;
+		var msg = printStatus(status);
+	}
+	
+	static function printStatus(status:HttpStatus) {
+		return switch(status) {
+			case NotFound: "Not found";
+			case MethodNotAllowed: "Method not allowed";
+		}
+	}
+}
