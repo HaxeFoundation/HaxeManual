1c980ac16d9aa9c9cdb685b9019940c3dace2fe2
Simon Krajewski <simon@haxe.org>
2013-11-22 10:24:24 +0100

    document new and inline new

---
+++ assets/NewInline.hx
@@ -0,0 +1,15 @@
+class Point {
+	public var x:Float;
+	public var y:Float;
+	
+	public inline function new(x:Float, y:Float) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class Main {
+	static public function main() {
+		var pt = new Point(1.2, 9.3);
+	}
+}
