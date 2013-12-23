0ea4dfedfe2565e35d85a8e5bad317b560347856
Miha <mlunar@gmail.com>
2013-12-23 20:38:30 +0100

    label fix and resolution order diagram plus minor changes
    fixed label from type-systemresolution-order to type-system-resolution-order
    added resolution order diagram based on Simn's graphviz one
    added underscore package to markdown latex template
    extracted the flowchart arrow style into a named style for general usage
    added simple preview batch file
    added a couple of latex files to ignore

---
+++ markdownPreview.bat
@@ -0,0 +1,4 @@
+:: Starts local GitHub Readme Instant Preview and opens introduction markdown file
+:: Requires https://github.com/joeyespo/grip to work
+start grip
+start http://localhost:5000/md/manual/introduction.md
