3fd8493b329466add930c780efc1f3417d8e611e
Simon Krajewski <simon@haxe.org>
2014-01-16 16:46:51 +0100

    handle some \todos

---
+++ assets/InterfaceWithVariables.hx
@@ -0,0 +1,10 @@
+interface Placeable {
+	public var x:Float;
+	public var y:Float;
+}
+
+class Main implements Placeable {
+	public var x:Float;
+	public var y:Float;
+	static public function main() { }
+}
