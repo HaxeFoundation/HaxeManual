54b46e6942f2d001ff10038183456d7f17f3e62e
Simon Krajewski <simon@haxe.org>
2014-02-27 01:19:19 +0100

    update extractor documentation

--- assets/Extractor.hx
+++ assets/Extractor.hx
@@ -7,7 +7,7 @@ class Main {
 	static public function main() {
 		var e = TString("fOo");
 		var success = switch(e) {
-			case TString(toLowerCase => "foo"): true;
+			case TString(_.toLowerCase() => "foo"): true;
 			case _: false;
 		}
 	}
