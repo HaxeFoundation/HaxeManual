680c1f306f75043d8142bab52adc0244704dce0b
Simon Krajewski <simon@haxe.org>
2014-03-31 00:40:27 +0200

    update

---
+++ assets/SafeCast.hx
@@ -0,0 +1,16 @@
+class Base {
+	public function new() { }
+}
+
+class Child1 extends Base {}
+
+class Child2 extends Base {}
+
+class Main {
+    public static function main() {
+		var child1:Base = new Child1();
+		var child2:Base = new Child2();
+		cast(child1, Base);
+		cast(child1, Child2); // Exception: Class cast error
+    }
+}
