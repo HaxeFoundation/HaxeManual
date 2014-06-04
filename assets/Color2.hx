cd747c1a0ad89e1942f8aa4b7ab748c398737de8
Simon Krajewski <simon@haxe.org>
2014-06-04 09:15:44 +0200

    give short introduction to enum usage

---
+++ assets/Color2.hx
@@ -0,0 +1,22 @@
+enum Color {
+	Red;
+	Green;
+	Blue;
+	Rgb(r:Int, g:Int, b:Int);
+}
+
+class Main {
+	static function main() {
+		var color = getColor();
+		switch (color) {
+			case Red: trace("Color was red");
+			case Green: trace("Color was green");
+			case Blue: trace("Color was blue");
+			case Rgb(r, g, b): trace("Color had a red value of " +r);
+		}
+	}
+
+	static function getColor():Color {
+		return Rgb(255, 0, 255);
+	}
+}
