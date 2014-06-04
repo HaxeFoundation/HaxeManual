ac98a63fa0b11dedf66a9855b21fcc34b0c60abd
Simon Krajewski <simon@haxe.org>
2014-06-04 10:41:21 +0200

    comment failing line in ImplementsDynamic example

--- assets/ImplementsDynamic.hx
+++ assets/ImplementsDynamic.hx
@@ -8,6 +8,6 @@ class Main {
 		var c = new ImplementsDynamic();
 		c.present = 1; // valid, present is an existing field
 		c.stringField = "foo"; // valid, assigned value is a String
-		c.intField = 1; // error, Int should be String
+		//c.intField = 1; // error, Int should be String
 	}
 }
