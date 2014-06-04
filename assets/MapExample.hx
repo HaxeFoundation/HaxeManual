4959c7aa9e9d4ecad5c167627cd974eb2f512d65
Simon Krajewski <simon@haxe.org>
2014-06-04 13:11:55 +0200

    format Map section

--- assets/MapExample.hx
+++ assets/MapExample.hx
@@ -1,32 +1,34 @@
-class MapExample{
-	static public function main(){
-		//Maps are initialized like arrays, but use '=>' operator
-		//Maps can have their key value types defined explicity
-		var map1 : Map<Int, String> = [1 => "one", 2=>"two"];
+class Main {
+	static public function main() {
+		// Maps are initialized like arrays, but use '=>' operator
+		// Maps can have their key value types defined explicity
+		var map1:Map<Int, String> = [1 => "one", 2=>"two"];
 
-		//Or they can infer their key value types
+		// Or they can infer their key value types
 		var map2 = ["one"=>1, "two"=>2, "three"=>3];
 		$type(map2); // Map<String, Int>
 
-		//Keys must be unique
+		// Keys must be unique
 		//var map3 = [1=>"dog", 1=>"cat"]; //Error: Duplicate Key
 
-		//Maps values can be accessed using array accessors "[]"
+		// Maps values can be accessed using array accessors "[]"
 		var map4 = ["M"=>"Monday", "T"=>"Tuesday"];
 		trace(map4["M"]); //Monday
 
-		//Maps iterate over their values by default 
+		// Maps iterate over their values by default
 		var valueSum;
-		for (value in map4)
-			trace(value); //Monday \n Tuesday 
+		for (value in map4) {
+			trace(value); // Monday \n Tuesday
+		}
 
-		//Can iterate over keys by using the keys() method
-		for (key in map4.keys())
-			trace(key);//M \n T 
+		// Can iterate over keys by using the keys() method
+		for (key in map4.keys()) {
+			trace(key); // M \n T
+		}
 
-        //Like arrays, a new map can be made using comprehension
+        // Like arrays, a new map can be made using comprehension
         var map5 = [for (key in map4.keys()) key => "FRIDAY!!"];
-        trace(map5); //{M => FRIDAY!!, T => FRIDAY!!}
+        trace(map5); // {M => FRIDAY!!, T => FRIDAY!!}
 	}
 }
-		
+
