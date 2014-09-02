1b10274ec6b6ba2146f2a87858b0df76b5e56e49
Simon Krajewski <simon@haxe.org>
2014-09-02 23:35:07 +0200

    explicitly mention the Dynamic vs. var inference case (see https://github.com/HaxeFoundation/haxe/issues/3297)

---
+++ assets/DynamicInferenceIssue.hx
@@ -0,0 +1,12 @@
+class Main {
+  static function main() {
+	var jsonData = '{ "Array": [1, 2, 3] }';
+    var json = haxe.Json.parse(jsonData);
+    $type(json); // Unknown<0>
+    for (i in 0...json.length) {
+	  // Array access is not allowed on
+	  // {+ length : Int }
+      trace(json[0]);
+    }
+  }
+}
