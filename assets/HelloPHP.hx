c38ebb428f86257681b8ff0472f063b84c094d3f
Mark Knol <mark.knol@gmail.com>
2015-02-05 22:44:04 +0100

    Update HelloPHP.hx
    Simpler

--- assets/HelloPHP.hx
+++ assets/HelloPHP.hx
@@ -2,21 +2,6 @@ import php.Lib;
 
 class Main {
 	static function main() {
-		var title = 'Haxe is great!';
-		var body = 'Hello world';
-		var html = '
-			<!DOCTYPE html>
-			<html>
-				<head>
-					<title>$title</title>
-				</head>
-				<body>
-					<h1>$title</h1>
-					<p>$body</p>
-				</body>
-			</html>
-		';
-		
-		Lib.println(html);
+		Lib.println('Haxe is great!');
 	}
 }
