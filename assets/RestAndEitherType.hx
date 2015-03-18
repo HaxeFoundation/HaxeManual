ec28541912ef57afa6ae1299cdf0b578dc8d75e0
Simon Krajewski <simon@haxe.org>
2015-03-18 09:16:19 +0100

    document haxe.extern.Rest/EitherType (closes #49)

---
+++ assets/RestAndEitherType.hx
@@ -0,0 +1,19 @@
+import haxe.extern.Rest;
+import haxe.extern.EitherType;
+
+extern class MyExtern {
+	static function f1(s:String, r:Rest<Int>):Void;
+	static function f2(e:EitherType<Int, String>):Void;
+}
+
+class Main {
+	static function main() {
+		MyExtern.f1("foo", 1, 2, 3); // use 1, 2, 3 as rest argument
+		MyExtern.f1("foo"); // no rest argument
+		//MyExtern.f1("foo", "bar"); // String should be Int
+
+		MyExtern.f2("foo");
+		MyExtern.f2(12);
+		//MyExtern.f2(true); // Bool should be EitherType<Int, String>
+	}
+}
