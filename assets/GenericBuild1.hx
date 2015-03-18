6fa5b741592fff7bdeb9d9b45baab2b03399d6ee
Simon Krajewski <simon@haxe.org>
2015-03-18 10:11:46 +0100

    document `@:genericBuild`

---
+++ assets/GenericBuild1.hx
@@ -0,0 +1,27 @@
+// MyMacro.hx
+import haxe.macro.Expr;
+import haxe.macro.Context;
+import haxe.macro.Type;
+
+class MyMacro {
+	static public function build() {
+		switch (Context.getLocalType()) {
+			case TInst(_, [t1]):
+				trace(t1);
+			case t:
+				Context.error("Class expected", Context.currentPos());
+		}
+		return null;
+	}
+}
+
+// Main.hx
+@:genericBuild(MyMacro.build())
+class MyType<T> { }
+
+class Main {
+	static function main() {
+		var x:MyType<Int>;
+		var x:MyType<String>;
+	}
+}
---
+++ assets/GenericBuild2.hx
@@ -0,0 +1,25 @@
+import haxe.macro.Expr;
+import haxe.macro.Context;
+import haxe.macro.Type;
+
+class MyMacro {
+	static public function build() {
+		switch (Context.getLocalType()) {
+			case TInst(_, [TInst(_.get() => { kind: KExpr(macro $v{(s:String)}) },_)]):
+				trace(s);
+			case t:
+				Context.error("Class expected", Context.currentPos());
+		}
+		return null;
+	}
+}
+
+// Main.hx
+@:genericBuild(MyMacro.build())
+class MyType<Const> { }
+
+class Main {
+	static function main() {
+		var x:MyType<"myfile.txt">;
+	}
+}
