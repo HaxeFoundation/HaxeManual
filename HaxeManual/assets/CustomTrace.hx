c5245807de284dbbcfeb25fb75abd838af0d2afb
Mark Knol <mark.knol@gmail.com>
2016-06-24 16:10:19 +0200

    Update CustomTrace.hx

--- HaxeManual/assets/CustomTrace.hx
+++ HaxeManual/assets/CustomTrace.hx
@@ -1,7 +1,7 @@
 class Main {
   function foo() {
     haxe.Log.trace = function(v:Dynamic, ?infos:haxe.PosInfos) { 
-      custom trace function here
+      //custom trace function here
     }
     trace("hello","warning",123);
   }
