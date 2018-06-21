340ef45b087050776cc53fa9a80750b8a318f3da
Guillaume Ambrois <Epono@users.noreply.github.com>
2018-06-21 15:05:56 +0200

    Fix of the doc for Map Comprehension (#341)
    * Fix doc for Map Comprehension
    
    * Fix doc for Map Comprehension

--- HaxeManual/assets/MapComprehension.hx
+++ HaxeManual/assets/MapComprehension.hx
@@ -1,10 +1,10 @@
 class Main {
   static public function main() {
-    var a = [for (i in 0...10) i];
-    trace(a); // [0,1,2,3,4,5,6,7,8,9]
-
+    var a = [for (i in 0...5) i => 'number ${i}'];
+    trace(a); // {0 => number 0, 1 => number 1, 2 => number 2, 3 => number 3, 4 => number 4}
+    
     var i = 0;
-    var b = [while(i < 10) i++];
-    trace(b); // [0,1,2,3,4,5,6,7,8,9]
+    var b = [while(i < 5) i => 'number ${i++}'];
+    trace(b); // {0 => number 0, 1 => number 1, 2 => number 2, 3 => number 3, 4 => number 4}
   }
 }
