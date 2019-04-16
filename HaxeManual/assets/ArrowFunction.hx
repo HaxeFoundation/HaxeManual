b98e232406e05b32520fe32c4ebde5b0c9d34b9c
Aurel Bílý <aurel.bily@gmail.com>
2019-04-16 12:30:45 +0100

    arrow functions

---
+++ HaxeManual/assets/ArrowFunction.hx
@@ -0,0 +1,15 @@
+class Main {
+  static public function main() {
+    var myConcat = (a : String, b : String) -> a + b;
+    var myChar = (a : String, b : Int) -> (a.charAt(b) : String);
+    $type(myConcat); // (a : String, b : String) -> String
+    $type(myChar); // (a : String, b : Int) -> String
+    trace(myConcat("foo", "bar")); // "foobar"
+    trace(myChar("example", 1)); // "x"
+    var myContains = (a : String, needle : String) -> {
+        if (a.indexOf(needle) == -1) return false;
+        trace(a, needle);
+        true;
+      };
+  }
+}
