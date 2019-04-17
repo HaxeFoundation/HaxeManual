70caf8c8f6f0b73b9da93acb256a458867d78853
Aurel Bílý <aurel.bily@gmail.com>
2019-04-17 13:18:25 +0100

    haxe.ds.List

--- HaxeManual/assets/ListExample.hx
+++ HaxeManual/assets/ListExample.hx
@@ -1,6 +1,6 @@
 class Main {
   static public function main() {
-    var myList = new List<Int>();
+    var myList = new haxe.ds.List<Int>();
     for (ii in 0...5)
       myList.add(ii);
     trace(myList); //{0, 1, 2, 3, 4}
