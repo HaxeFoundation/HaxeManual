2ddc70154c28e5cd3e1793af63845b608c5bac74
Aurel Bílý <aurel.bily@gmail.com>
2019-04-17 22:02:53 +0100

    sys and threading

---
+++ HaxeManual/assets/Threads.hx
@@ -0,0 +1,13 @@
+class Main {
+  public static function main():Void {
+#if (target.threaded)
+    sys.thread.Thread.create(() -> {
+        while (true) {
+          trace("other thread");
+          Sys.sleep(1);
+        }
+      });
+    Sys.sleep(3);
+#end
+  }
+}
