840623866573d284bcb26d36c6b8bc0c663c9d1c
John Gabriele <jgabriele@fastmail.fm>
2019-10-16 00:34:48 -0400

    asset: SafeCast.hx, clarify which line throws

--- assets/SafeCast.hx
+++ assets/SafeCast.hx
@@ -9,8 +9,7 @@ class Main {
   public static function main() {
     var child1:Base = new Child1();
     var child2:Base = new Child2();
-    cast(child1, Base);
-    // Exception: Class cast error
-    cast(child1, Child2);
+    cast(child1, Base);   // Ok
+    cast(child1, Child2); // Exception: Class cast error
   }
 }
