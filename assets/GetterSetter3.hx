50c5b67a407e3375f65dbb168ab96c94736e58aa
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2022-02-25 16:47:49 +0800

    Missing colon (#504)

--- assets/GetterSetter3.hx
+++ assets/GetterSetter3.hx
@@ -1,5 +1,5 @@
 class Main {
-  // @isVar forces the field to be physical allowing the program to compile.
+  // @:isVar forces the field to be physical allowing the program to compile.
   @:isVar public var x(get, set):Int;
 
   function get_x() {
